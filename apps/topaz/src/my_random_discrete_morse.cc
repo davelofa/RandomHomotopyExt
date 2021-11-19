478602ff0c2dbba95ab812edd9b727c153041f63
davelofa <39552204+davelofa@users.noreply.github.com>
2021-11-19 12:03:30 +0100

    Update my_random_discrete_morse.cc

--- apps/topaz/src/my_random_discrete_morse.cc
+++ apps/topaz/src/my_random_discrete_morse.cc
@@ -1,6 +1,6 @@
- /* Copyright (c) 2014
-   Michael Joswig, Frank Lutz, Mimi Tsuruga
-   tsuruga@math.tu-berlin.de
+ /* Copyright (c) 2021
+   Michael Joswig, Frank Lutz, Mimi Tsuruga, Davide Lofano
+   lofano@math.tu-berlin.de
    This program is free software; you can redistribute it and/or modify it
    under the terms of the GNU General Public License as published by the
    Free Software Foundation; either version 2, or (at your option) any
