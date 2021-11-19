b8533730b6cff7fb1ee8b6852def8f12d7425efc
davelofa <39552204+davelofa@users.noreply.github.com>
2021-11-19 12:04:18 +0100

    Update R_H2.cc

--- apps/topaz/src/R_H2.cc
+++ apps/topaz/src/R_H2.cc
@@ -1,3 +1,19 @@
+ /* Copyright (c) 2021
+   Davide Lofano
+   lofano@math.tu-berlin.de
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2, or (at your option) any
+   later version: http://www.gnu.org/licenses/gpl.txt.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+--------------------------------------------------------------------------------
+   $Project: polymake $$Id$
+*/
+
+
 #include "polymake/Rational.h"
 #include "polymake/topaz/complex_tools.h"
 #include "polymake/Matrix.h"
